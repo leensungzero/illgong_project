10b41d6e3f21fbe64f0f306dea8c9dc969145c17
leensungzero <brianwoo119@naver.com>
2019-08-25 23:41:06 +0900

    MOD: Change User model for evincive interface

--- member/models/user.py
+++ member/models/user.py
@@ -3,7 +3,18 @@ from django.contrib.auth.models import AbstractBaseUser
 
 
 class User(AbstractBaseUser):
-    email = models.EmailField(unique=True)
+    email = models.EmailField(
+        unique=True,
+        verbose_name='이메일',
+    )
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f'User(ID {self.id})'
+
+    class Meta:
+        db_table = 'user'
+        verbose_name = '유저'
+        verbose_name_plural = '{} {}'.format(verbose_name, '목록')
