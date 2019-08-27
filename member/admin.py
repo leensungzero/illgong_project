679f1ff66cdcc17165a8df35bd30eee3c20fa967
leensungzero <brianwoo119@naver.com>
2019-08-27 11:26:19 +0900

    ADD: Register ( User, UserProfile ) admin

--- member/admin.py
+++ member/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+from member.models.user_profile import UserProfile
+
+
+User = get_user_model()
+
+
+admin.site.register(User)
+admin.site.register(UserProfile)
